23a4540f0b5a7cd2f50114cf31802c9ff8748b57
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-10-24 18:45:42 +0200

    feat(server): replace index ejs view with webpack index result

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const DataStore = require('nedb');
 const db = new DataStore({ filename: 'db.db', autoload: true });
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client-dist'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
+
 app.post('/', (req, res) => {
     req.sanitizeBody('message').escape();
-
     if (req.body.message) {
 
         db.insert({
@@ -26,10 +24,6 @@ app.post('/', (req, res) => {
             renderIndex(res);
         });
     }
-});
-
-app.get('/', (req, res) => {
-    renderIndex(res);
 });
 
 const port = process.env.port || 3000;
